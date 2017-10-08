c6029b1b89c70dfe1ba8242129fc2e208b6decd8
Robert Edwards <raedwards@gmail.com>
2017-10-08 17:30:48 +0200

    updating to FastTree

--- bin/rename_trees.py
+++ bin/rename_trees.py
@@ -16,11 +16,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse a tree')
     parser.add_argument('-t', help='tree file', required=True)
     parser.add_argument('-i', help='id map file', required=True)
-    parser.add_argument('-n', help='name of the tag to use in the label. Valid tags are {}'.format(tags), default='id')
+    parser.add_argument('-n', help='name of the tag to use in the label. You can provide multiple tags and they will be joined with _ Valid tags are {}'.format(tags), action='append')
     args = parser.parse_args()
 
-    if args.n.lower() not in tags:
-        sys.exit('Sorry {} is not a valid tag. It must be one of {} '.format(args.n, tags))
+    for n in args.n:
+        if n.lower() not in tags:
+            sys.exit('Sorry {} is not a valid tag. It must be one of {} '.format(args.n, tags))
 
     idmap = {}
     tagcount = {}
@@ -32,17 +33,23 @@ if __name__ == '__main__':
             else:
                 m = re.findall('\[(\S+)\=(.*?)\]', p[1])
                 tagdata = {t[0]:t[1] for t in m}
-                if args.n.lower() in tagdata:
-                    tagcount[tagdata[args.n]] = tagcount.get(tagdata[args.n], 0)+1
-                    idmap[p[0]] = "_".join(map (str, [tagdata[args.n], tagcount[tagdata[args.n]]]))
-                else:
-                    if args.n.lower() != 'id':
-                        sys.stderr.write("No {} data found in sample: {}\n".format(args.n, l.strip()))
+                tagarr = []
+                for n in args.n:
+                    if n.lower() in tagdata:
+                        tagarr.append(tagdata[n.lower()])
+                tagstring = "_".join(tagarr)
+
+                if '' == tagstring:
+                    sys.stderr.write("No {} data found in sample: {}\n".format(args.n, l.strip()))
                     idmap[p[0]] = p[1].split()[0]
+                else:
+                    tagcount[tagstring] = tagcount.get(tagstring, 0)+1
+                    idmap[p[0]] = "{}_{}".format(tagstring, tagcount[tagstring])
 
     tree = Tree(args.t)
     for leaf in tree:
         oldname = leaf.name
+        oldname = oldname.replace('_R_', '')
         if oldname not in idmap:
             sys.stderr.write("ERROR: {} is not in {} as an ID\n".format(oldname, args.i))
             continue
