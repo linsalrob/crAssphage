b2d11ff1aaccab37823fb469d8d35d9efab92764
linsalrob <raedwards@gmail.com>
2017-10-09 05:27:32 -0700

    New trees of all the data

--- Combined_Analysis/Analysis/PrimerC/muscle.sh
+++ Combined_Analysis/Analysis/PrimerC/muscle.sh
@@ -1,1 +1,4 @@
-/home3/redwards/opt/muscle/muscle -in seqs.C.renum.rc.fa -out seqs.C.rc.aln -maxiters 2 -diags
+export PRIMER=C
+/home3/redwards/opt/muscle/muscle -in seqs.$PRIMER.renum.rc.fa -out seqs.$PRIMER.renum.rc.aln -maxiters 2 -diags
+python3 ../../../bin/trim_fasta_alignment.py -f seqs.$PRIMER.renum.rc.aln -c 0.9 -r 0.8 -v > seqs.$PRIMER.rc.trim.aln
+/home3/redwards/opt/FastTree/fasttree -nt seqs.$PRIMER.rc.trim.aln > seqs.$PRIMER.rc.trim.tree
