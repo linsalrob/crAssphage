95a5e5099979b8b54a2aadd41e9b98d1f82f62a6
linsalrob <raedwards@gmail.com>
2017-10-09 03:15:45 -0700

    Remaking trees with muscle

--- Combined_Analysis/Analysis/PrimerC/muscle.sh
+++ Combined_Analysis/Analysis/PrimerC/muscle.sh
@@ -1,1 +1,1 @@
-/home3/redwards/opt/muscle/muscle -in seqs.C.rc.fa -out seqs.C.rc.aln -maxiters 2 -diags
+/home3/redwards/opt/muscle/muscle -in seqs.C.renum.rc.fa -out seqs.C.rc.aln -maxiters 2 -diags
