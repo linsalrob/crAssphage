98788286f7fd563ac23660b7cad2a85e6d210e19
Robert Edwards <raedwards@gmail.com>
2017-10-28 13:09:41 -0700

    renaming and generating metadata

--- bin/metadata.py
+++ bin/metadata.py
@@ -2,7 +2,7 @@
 Create a tab separated file of metadata with column headers that start
 with a #.
 
-We read the fasta file and make the file for every sequence, making sure
+We read the id map file and make an entry for every sequence, making sure
 to check for missing data
 """
 
@@ -16,42 +16,34 @@ __author__ = 'Rob Edwards'
 
 
 
-def parse_fasta(fafile):
+def parse_fasta(idfile):
     """
     Parse the fasta file and return a hash of sequence ids and metadata
     
     We also do some sanity checks on the data
     """
-    
     seqids = set()
     data = OrderedDict()
     fatalerror = False
 
-    with open(fafile, 'r') as f:
+    with open(idfile, 'r') as f:
         for l in f:
-            if not l.startswith('>'):
-                continue
-            seqid=l.split(' ')[0].replace('>', '', 1)
-            if (seqid in seqids):
-                sys.stderr.write("FATAL: Duplicate sequence ID: {} in {}\n".format(seqid, fafile))
-                fatalerror = True
-            seqids.add(seqid)
-            for meta in re.findall('\\[(.*?)\\]', l):
+            p=l.strip().split("\t")
+            seqids.add(p[0])
+            for meta in re.findall('\\[(.*?)\\]', p[1]):
                 tag,value=meta.split('=')
                 if tag not in data:
                     data[tag] = {}
-                data[tag][seqid]=value
+                data[tag][p[0]]=value
 
 
         # now we have read the file, lets check a few things out
         
-        wantedkeys = ['latitude', 'longitude', 'latlon', 'city', 'country']
+        wantedkeys = ['latitude', 'longitude', 'latlon', 'locality', 'country']
         for w in wantedkeys:
-
             if w not in data:
-                sys.stderr.write("FILE ERROR: No {} entries were found in {}\n".format(w, fafile))
+                sys.stderr.write("FILE ERROR: No {} entries were found in {}\n".format(w, idfile))
                 continue
-
             for s in seqids:
                 if s not in data[w]:
                     sys.stderr.write("ENTRY ERROR: No {} was found for {}\n".format(w, s))
@@ -79,11 +71,11 @@ def print_table(seqids, data, outputfile, separator='\t'):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Make a metadata file from a fasta file')
-    parser.add_argument('-f', help='fasta file to make the metadata from', required=True)
+    parser = argparse.ArgumentParser(description='Make a metadata file from the id.map file')
+    parser.add_argument('-i', help='id.map file to make the metadata from', required=True)
     parser.add_argument('-o', help='output file', required=True)
     args = parser.parse_args()
 
-    seqids, data = parse_fasta(args.f)
+    seqids, data = parse_fasta(args.i)
     print_table(seqids, data, args.o)
 
---
+++ bin/rename_fasta.py
@@ -0,0 +1,118 @@
+"""
+Rename our fasta files using locality:country:date:number format
+
+Locality is probably (but not always) city, and matches the regexp [a-zA-Z0-9_] (i.e. \w+)
+Country is the country and also matches the \w regexp
+data is always an eight digit number e.g. 20171028 for October 28 2017 or 00000000 if we don't have the date
+number is a number to disambiguate different samples from the same city on the same day.
+
+The identifier can be separated with either:
+    split(":")
+    or
+    \w+:\w+:\d{8}:\d+
+
+"""
+
+import os
+import sys
+import argparse
+import re
+
+def stripout(txt):
+    """
+    Replace white space and non \w characters in text
+    :param txt: some text to clean
+    :return: clean text
+    """
+    txt = txt.replace(' ', '_')
+    return re.sub('\W', '', txt)
+
+
+def rename(fafile, idmapfile, outputfa):
+    """
+    Rename the sequences
+    :param fafile: input fasta file
+    :param idmapfile: output id.map file with new name and original name
+    :param outputfa: output fasta file
+    :return: None
+    """
+
+    counts = {}
+
+    idout = open(idmapfile, 'w')
+    faout = open(outputfa, 'w')
+    fatal_error = False
+    notfirstsequence = False # we don't want to print a new line for the first sequence
+    with open(fafile, 'r') as fa:
+        for l in fa:
+            l=l.strip()
+            lcl = 'NA'
+            cnt = 'NA'
+            dt = '00000000'
+            if l.startswith('>'):
+                if notfirstsequence:
+                    faout.write("\n")
+                else:
+                    notfirstsequence = True
+                l=l.replace('>', '', 1)
+                if 'locality' in l:
+                    m = re.search('\[locality=(.*?)\]', l)
+                    if not m:
+                        sys.stderr.write("Even though its there, can't parse locality in {}\n".format(l))
+                        fatal_error = True
+                    lcl = stripout(m.groups()[0])
+                else:
+                    sys.stderr.write("FATAL ERROR: Locality was not found in {}\n".format(l))
+                    fatal_error = True
+
+                if 'country' in l:
+                    m = re.search('\[country=(.*?)\]', l)
+                    if not m:
+                        sys.stderr.write("Even though its there, can't parse country in {}\n".format(l))
+                        fatal_error = True
+                    cnt = stripout(m.groups()[0])
+                else:
+                    sys.stderr.write("FATAL ERRROR: Country was not found in {}\n".format(l))
+                    fatal_error = True
+
+
+                if 'date' in l:
+                    m = re.search('\[date=(.*?)\]', l)
+                    if not m:
+                        sys.stderr.write("Even though its there, can't parse country in {}\n".format(l))
+                        fatal_error = True
+                    dt = m.groups()[0]
+                    if re.search('\D', dt):
+                        sys.stderr.write("WARNING: the date in {} has non-digit entries\n".format(l))
+                        dt = re.sub('\D', '', dt)
+                    if not re.match('\d{8}', dt):
+                        sys.stderr.write("WARNING the date in {} is not correct\n".format(l))
+                        while not re.match('\d{8}', dt):
+                            dt += '0'
+
+                identifier = "{}:{}:{}:".format(lcl, cnt, dt)
+
+                counts[identifier] = counts.get(identifier, 0) + 1
+                identifier += str(counts[identifier])
+
+                idout.write("{}\t{}\n".format(identifier, l))
+                faout.write(">{}\n".format(identifier))
+            else:
+                faout.write(l)
+    faout.write("\n")
+
+    idout.close()
+    faout.close()
+
+    if fatal_error:
+        sys.exit(-1)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-f', help="fasta file to rename", required=True)
+    parser.add_argument('-o', help='output fasta file', required=True)
+    parser.add_argument('-i', help='output id.map file that has the original information and the new information', required=True)
+    parser.add_argument('-v', help='verbose output', action='store_true')
+    args = parser.parse_args()
+
+    rename(args.f, args.i, args.o)
