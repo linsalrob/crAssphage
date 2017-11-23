fc0cee677ea84df77dce5cc8630c107b8889d2c4
linsalrob <raedwards@gmail.com>
2017-11-23 13:25:28 -0800

    combined data

--- Combined_Analysis/Analysis/PrimerA/muscle.sh
+++ Combined_Analysis/Analysis/PrimerA/muscle.sh
@@ -1,4 +1,3 @@
 export PRIMER=A
 /home3/redwards/opt/muscle/muscle -in seqs.$PRIMER.renum.rc.fa -out seqs.$PRIMER.renum.rc.aln -maxiters 2 -diags
 python3 ../../../bin/trim_fasta_alignment.py -f seqs.$PRIMER.renum.rc.aln -c 0.9 -r 0.8 -v > seqs.$PRIMER.rc.trim.aln
-/home3/redwards/opt/FastTree/fasttree -nt seqs.$PRIMER.rc.trim.aln > seqs.$PRIMER.rc.trim.fasttree
