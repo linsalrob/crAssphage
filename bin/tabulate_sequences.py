16800fbc4bbab17e64ef35522a987aa5e209a7ff
Robert Edwards <raedwards@gmail.com>
2018-04-05 17:12:14 -0700

    Tabulating metadata

--- bin/tabulate_sequences.py
+++ bin/tabulate_sequences.py
@@ -15,64 +15,95 @@ import re
 __author__ = 'Rob Edwards'
 
 
+def read_fasta(fname):
+    """
+    Read a fasta file and return a hash.
+    :param fname: The file name to read
+    :return: dict
+    """
 
-def parse_fasta(idfile):
+    seqs = {}
+    seq = ""
+    seqid = ""
+    with open(fname, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith(">"):
+                if "" != seqid:
+                    seqs[seqid] = seq
+                seqid = line
+                seq = ""
+            else:
+                seq += line
+        seqs[seqid] = seq
+    return seqs
+
+
+
+
+def parse_fasta(fafile):
     """
     Parse the fasta file and return a hash of sequence ids and metadata
     
     We also do some sanity checks on the data
+    :param fafile: the fasta file to check
+    :return: dict, dict
     """
-    seqids = set()
+
     data = OrderedDict()
+    data['seqid']={}
     fatalerror = False
 
-    with open(idfile, 'r') as f:
-        for l in f:
-            p=l.strip().split("\t")
-            seqids.add(p[0])
-            for meta in re.findall('\\[(.*?)\\]', p[1]):
-                tag,value=meta.split('=')
-                if tag not in data:
-                    data[tag] = {}
-                data[tag][p[0]]=value
+    seqs = read_fasta(fafile)
+    for l in seqs:
+        seqid = l.split(" ")[0].replace(">", "", 1)
+        data["seqid"][l]=seqid
+        for meta in re.findall('\[(.*?)\]', l):
+            tag,value=meta.split('=')
+            if 'notes' == tag:
+                tag = 'note'
+            if tag not in data:
+                data[tag] = {}
+            data[tag][l]=value
 
 
-        # now we have read the file, lets check a few things out
-        
-        wantedkeys = ['latitude', 'longitude', 'latlon', 'locality', 'country']
-        for w in wantedkeys:
-            if w not in data:
-                sys.stderr.write("FILE ERROR: No {} entries were found in {}\n".format(w, idfile))
-                continue
-            for s in seqids:
-                if s not in data[w]:
-                    sys.stderr.write("ENTRY ERROR: No {} was found for {}\n".format(w, s))
+    # now we have read the file, lets check a few things out
+    wantedkeys = ['latitude', 'longitude', 'latlon', 'locality', 'country']
+    for w in wantedkeys:
+        if w not in data:
+            sys.stderr.write("FILE ERROR: No {} entries were found in {}\n".format(w, fafile))
+            continue
+        c=0
+        for s in seqs:
+            c+=1
+            if c > 10:
+                break
+            if s not in data[w]:
+                sys.stderr.write("ENTRY ERROR: No {} was found for {}\n".format(w, s))
 
-    return seqids, data
+    return seqs, data
 
 
-def print_table(seqids, data, outputfile, separator='\t'):
+def print_table(seqs, data, outputfile, separator='\t'):
     """
     Write the metadata to a tab separated text file
     """
 
-    tags = data.keys()
+    tags = [x for x in data.keys() if x != 'seqid']
     with open(outputfile, 'w') as out:
-        out.write(separator.join(["#Sequence ID"] + list(tags)) + "\n")
-        for s in seqids:
-            out.write(s)
+        out.write(separator.join(["#Sequence ID"] + tags + ["Sequence\n"]))
+        for s in seqs:
+            out.write(data['seqid'][s])
             for t in tags:
                 out.write("{}{}".format(separator, data[t].get(s, "")))
-            out.write("\n")
-
-
+            out.write("{}{}\n".format(separator, seqs[s].upper()))
 
 
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Make a metadata file from the id.map file')
-    parser.add_argument('-i', help='id.map file to make the metadata from', required=True)
+    parser = argparse.ArgumentParser(description='Make a metadata file that inlcudes all metadata and the DNA sequence from a fasta file')
+    parser.add_argument('-i', help='fasta file to make the metadata from', required=True)
     parser.add_argument('-o', help='output file', required=True)
     args = parser.parse_args()
 
