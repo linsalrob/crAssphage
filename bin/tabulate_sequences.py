bf83a32403ab1c106f4d662e00dae45b10f8f589
Robert Edwards <raedwards@gmail.com>
2018-04-05 17:37:41 -0700

    adding the primer as an early and consistent column

--- bin/tabulate_sequences.py
+++ bin/tabulate_sequences.py
@@ -41,25 +41,33 @@ def read_fasta(fname):
 
 
 
-def parse_fasta(fafile):
+def parse_fasta(fafile, primer):
     """
     Parse the fasta file and return a hash of sequence ids and metadata
     
     We also do some sanity checks on the data
     :param fafile: the fasta file to check
+    :param primer: the primer used for the PCR
     :return: dict, dict
     """
 
     data = OrderedDict()
     data['seqid']={}
+    data['primer']={}
     fatalerror = False
 
     seqs = read_fasta(fafile)
     for l in seqs:
         seqid = l.split(" ")[0].replace(">", "", 1)
         data["seqid"][l]=seqid
+        data['primer'][l]=primer
         for meta in re.findall('\[(.*?)\]', l):
             tag,value=meta.split('=')
+            tag = tag.lower()
+            if 'primer' == tag:
+                if primer != value:
+                    sys.stderr.write("You told me the primer was {} but sample {} thinks the primer is {}\n".format(primer, l, value))
+                continue
             if 'notes' == tag:
                 tag = 'note'
             if tag not in data:
@@ -104,9 +112,10 @@ def print_table(seqs, data, outputfile, separator='\t'):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Make a metadata file that inlcudes all metadata and the DNA sequence from a fasta file')
     parser.add_argument('-i', help='fasta file to make the metadata from', required=True)
+    parser.add_argument('-p', help='primer used for this data', required=True)
     parser.add_argument('-o', help='output file', required=True)
     args = parser.parse_args()
 
-    seqids, data = parse_fasta(args.i)
+    seqids, data = parse_fasta(args.i, args.p)
     print_table(seqids, data, args.o)
 
