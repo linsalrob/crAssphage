0c91e78a28607a31affb144032da3d58dd695561
Robert Edwards <raedwards@gmail.com>
2018-04-08 09:19:21 -0700

    Consistent ordering and new id for iq-tree

--- bin/tabulate_sequences.py
+++ bin/tabulate_sequences.py
@@ -2,8 +2,10 @@
 Create a tab separated file of metadata with column headers that start
 with a #.
 
-We read the id map file and make an entry for every sequence, making sure
+We read the fasta file and the id map file and make an entry for every sequence, making sure
 to check for missing data
+
+
 """
 
 import os
@@ -31,7 +33,7 @@ def read_fasta(fname):
             if line.startswith(">"):
                 if "" != seqid:
                     seqs[seqid] = seq
-                seqid = line
+                seqid = line.replace('>', '', 1)
                 seq = ""
             else:
                 seq += line
@@ -41,45 +43,51 @@ def read_fasta(fname):
 
 
 
-def parse_fasta(fafile, primer):
+def parse_id_map(idmapfile, seqs, primer):
     """
     Parse the fasta file and return a hash of sequence ids and metadata
     
     We also do some sanity checks on the data
-    :param fafile: the fasta file to check
+    :param idmapfile: the fasta file to check
     :param primer: the primer used for the PCR
     :return: dict, dict
     """
 
     data = OrderedDict()
-    data['seqid']={}
-    data['primer']={}
+    for x in ['seqid', 'primer', "name", "date", "yearmonth", "lat_lon", "locality", "latitude", "longitude", "latlon", "altitude", "address", "country", "location", "contact", "description", "method", "note", "bin_id", "sample", "sample_date", "sampleid", "sample_number", "sample_processing", "sampletype", "sex", "site", "source", "type", "university", "volunteer"]:
+        data[x]={}
+
     fatalerror = False
 
-    seqs = read_fasta(fafile)
-    for l in seqs:
-        seqid = l.split(" ")[0].replace(">", "", 1)
-        data["seqid"][l]=seqid
-        data['primer'][l]=primer
-        for meta in re.findall('\[(.*?)\]', l):
-            tag,value=meta.split('=')
-            tag = tag.lower()
-            if 'primer' == tag:
-                if primer != value:
-                    sys.stderr.write("You told me the primer was {} but sample {} thinks the primer is {}\n".format(primer, l, value))
-                continue
-            if 'notes' == tag:
-                tag = 'note'
-            if tag not in data:
-                data[tag] = {}
-            data[tag][l]=value
+    with open(idmapfile, 'r') as f:
+        for l in f:
+            seqid=l.strip().split("\t")[0]
+            if seqid not in seqs:
+                sys.stderr.write("Sequence id {} was not found in the sequence file\n".format(seqid))
+                fatalerror = True
+
+            data["seqid"][seqid]=seqid
+            data['primer'][seqid]=primer
+            for meta in re.findall('\[(.*?)\]', l):
+                tag,value=meta.split('=')
+                tag = tag.lower()
+                if 'primer' == tag:
+                    if primer != value:
+                        sys.stderr.write("You told me the primer was {} but sample {} thinks the primer is {}\n".format(primer, l, value))
+                    continue
+                if 'notes' == tag:
+                    tag = 'note'
+                if tag not in data:
+                    sys.stderr.write("Added a tag ({}) that was not in our defined list. Order may be different for primers A, B, and C\n".format(tag))
+                    data[tag] = {}
+                data[tag][seqid]=value
 
 
     # now we have read the file, lets check a few things out
     wantedkeys = ['latitude', 'longitude', 'latlon', 'locality', 'country']
     for w in wantedkeys:
         if w not in data:
-            sys.stderr.write("FILE ERROR: No {} entries were found in {}\n".format(w, fafile))
+            sys.stderr.write("FILE ERROR: No {} entries were found in {}\n".format(w, idmapfile))
             continue
         c=0
         for s in seqs:
@@ -89,7 +97,7 @@ def parse_fasta(fafile, primer):
             if s not in data[w]:
                 sys.stderr.write("ENTRY ERROR: No {} was found for {}\n".format(w, s))
 
-    return seqs, data
+    return data
 
 
 def print_table(seqs, data, outputfile, separator='\t'):
@@ -99,9 +107,10 @@ def print_table(seqs, data, outputfile, separator='\t'):
 
     tags = [x for x in data.keys() if x != 'seqid']
     with open(outputfile, 'w') as out:
-        out.write(separator.join(["#Sequence ID"] + tags + ["Sequence\n"]))
+        out.write(separator.join(["#sequence ID", "alternate ID"] + tags + ["sequence\n"]))
         for s in seqs:
-            out.write(data['seqid'][s])
+            tempid = data['seqid'][s].replace('|', '_')
+            out.write("{}\t{}".format(tempid, data['seqid'][s]))
             for t in tags:
                 out.write("{}{}".format(separator, data[t].get(s, "")))
             out.write("{}{}\n".format(separator, seqs[s].upper()))
@@ -111,11 +120,13 @@ def print_table(seqs, data, outputfile, separator='\t'):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Make a metadata file that inlcudes all metadata and the DNA sequence from a fasta file')
-    parser.add_argument('-i', help='fasta file to make the metadata from', required=True)
+    parser.add_argument('-f', help='fasta file to read the sequences from', required=True)
+    parser.add_argument('-i', help='idmap to make the metadata from', required=True)
     parser.add_argument('-p', help='primer used for this data', required=True)
     parser.add_argument('-o', help='output file', required=True)
     args = parser.parse_args()
 
-    seqids, data = parse_fasta(args.i, args.p)
-    print_table(seqids, data, args.o)
+    seqs = read_fasta(args.f)
+    data = parse_id_map(args.i, seqs, args.p)
+    print_table(seqs, data, args.o)
 
