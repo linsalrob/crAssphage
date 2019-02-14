8352c96a42d98e6229e65751f72fc86deac85ed3
linsalrob <raedwards@gmail.com>
2019-02-13 19:11:11 -0800

    adding more colors

--- bin/map_drawing/crassphage_maps/__init__.py
+++ bin/map_drawing/crassphage_maps/__init__.py
@@ -1,9 +1,11 @@
-from .evenly_select import evenly_select
+from .colors import evenly_select
+from .colors import green2yellow, red2blue, green2red, GnBu5
 from .ll_to_distance import latlon2distance
-from .data import country2continent, green2yellow, red2blue, green2red
+from .data import country2continent
 from .read_files import get_lon_lat, closest_dna_dist
 __all__ = [
-    'evenly_select', 'latlon2distance',
-    'country2continent', 'green2yellow', 'red2blue', 'green2red',
+    'evenly_select',
+    'country2continent', 'latlon2distance',
+    'green2yellow', 'red2blue', 'green2red', 'GnBu5',
     'get_lon_lat', 'closest_dna_dist'
 ]
